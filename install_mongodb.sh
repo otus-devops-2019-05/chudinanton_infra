51252693cbcc34cf73716b46972e9c5449674a0c
Anton Chudin <chudinanton@gmail.com>
2019-03-25 00:18:15 +0300

    Fix error from Travis Build #14

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-echo "Install MongoDB Server"
-echo "Adding keys"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo "Adding repository"
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
-echo "Installing mongodb"
 sudo apt update
 sudo apt install -y mongodb-org
-echo "Starting service MongoDB"
 sudo systemctl start mongod
 sudo systemctl enable mongod
 echo "Done"
