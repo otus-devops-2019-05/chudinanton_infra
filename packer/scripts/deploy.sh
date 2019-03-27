1bff76a9d0bf9e8b36ac27055e3ae3665f73ed6e
Anton Chudin <chudinanton@gmail.com>
2019-03-27 10:53:04 +0300

    Fix errored: scp reddit.service: Permission denied

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -6,6 +6,7 @@ git clone -b monolith https://github.com/express42/reddit.git
 echo "Bundle installing"
 cd reddit/ && bundle install
 echo "Starting puma server"
+mv /home/chudinanton/reddit.service /etc/systemd/system/
 systemctl start reddit
 systemctl enable reddit
 echo "Done"
