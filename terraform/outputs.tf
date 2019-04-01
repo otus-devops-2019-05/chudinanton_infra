4b302568b43bc404d9824fe9afd151df08a72b53
Anton Chudin <chudinanton@gmail.com>
2019-04-01 16:00:18 +0300

    Add info

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "app_external_ip" {
   value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
 }
 
+output "app2_external_ip" {
+  value = "${google_compute_instance.app2.network_interface.0.access_config.0.nat_ip}"
+}
+
 output "balancer_ip" {
   value = "${google_compute_forwarding_rule.reddit_forwarding_rule.ip_address}"
 }
