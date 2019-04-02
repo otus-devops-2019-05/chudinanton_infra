169715e50032c7bf1a096e4aba77dc5140647efc
Anton Chudin <chudinanton@gmail.com>
2019-04-02 09:47:53 +0300

    Add firewall rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,3 +79,13 @@ resource "google_compute_firewall" "firewall_puma" {
   # Правило применимо для инстансов с перечисленными тэгами
   target_tags = ["reddit-app"]
 }
+
+resource "google_compute_firewall" "firewall_ssh" {  
+  name = "default-allow-ssh"  
+  network = "default"  
+  allow {    
+    protocol = "tcp"    
+    ports = ["22"]  
+    }  
+  source_ranges = ["0.0.0.0/0"]
+  }
