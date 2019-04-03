90eeea88b2be9c7a308f7dc66f732cd25cb5db4c
Anton Chudin <chudinanton@gmail.com>
2019-04-03 11:53:12 +0300

    add source

---
+++ terraform/prod/variables.tf
@@ -0,0 +1,45 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+
+  # Значение по умолчанию
+  default = "europe-west3"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable private_key_path {
+  # Описание переменной
+  description = "Path to the private key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+variable zone {
+  description = "Zone"
+  default     = "*"
+}
+variable public_key_path_appuser1 {
+  description = "Path to the public key used for ssh access appuser1"
+}
+variable public_key_path_appuser2 {
+  description = "Path to the public key used for ssh access appuser2"
+}
+variable public_key_path_appuser3 {
+  description = "Path to the public key used for ssh access appuser3"
+}
+variable count {
+  description = "count instances"
+  default     = "1"
+}
+variable app_disk_image {  
+  description = "Disk image for reddit app"  
+  default = "reddit-app-base"
+}
+variable db_disk_image {  
+  description = "Disk image for reddit db"  
+  default = "reddit-db-base"
+}
---
+++ terraform/stage/outputs.tf
@@ -0,0 +1,7 @@
+output "app_external_ip" {  
+  value = "${module.app.app_external_ip}"
+  }
+
+#output "balancer_ip" {
+#  value = "${google_compute_forwarding_rule.reddit_forwarding_rule.ip_address}"
+#}
