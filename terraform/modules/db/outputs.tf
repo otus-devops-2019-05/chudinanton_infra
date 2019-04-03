a195d9a0ec1177055ed72721bf28b05ae7e67928
Anton Chudin <chudinanton@gmail.com>
2019-04-03 11:05:11 +0300

    app_external_ip

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -1,3 +1,4 @@
 output "mongodb_external_ip" {
   value = "${google_compute_instance.db.network_interface.0.network_ip}"
 }
+
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "app_external_ip" {
-  value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
-}
+output "app_external_ip" {  
+  value = "${module.app.app_external_ip}"
+  }
 
 #output "balancer_ip" {
 #  value = "${google_compute_forwarding_rule.reddit_forwarding_rule.ip_address}"
