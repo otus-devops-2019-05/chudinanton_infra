294d7759fd20870f6e14805cd97212bfdcc39f1b
Anton Chudin <chudinanton@gmail.com>
2019-04-03 10:57:52 +0300

    Add app module and add links in main.tf

---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,16 @@
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable zone {
+  description = "Zone"
+  default     = "*"
+}
+variable count {
+  description = "count instances"
+  default     = "1"
+}
+variable app_disk_image {  
+  description = "Disk image for reddit app"  
+  default = "reddit-app-base"
+}
