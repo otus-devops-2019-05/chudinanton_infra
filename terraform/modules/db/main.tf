c132e55ec916cdc5d19ce365758c0e0771ffe633
Anton Chudin <chudinanton@gmail.com>
2019-04-03 10:45:33 +0300

    Add db module

---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,43 @@
+resource "google_compute_instance" "db" {  
+    name = "reddit-db"  
+    machine_type = "g1-small"  
+    zone = "${var.zone}"    
+    tags = ["reddit-db"]    
+    
+    boot_disk {    
+        initialize_params {      
+            image = "${var.db_disk_image}"    
+        }  
+    }  
+    
+    network_interface {    
+        network = "default"    
+        access_config = {}  
+    }  
+    
+    metadata {    
+        ssh-keys = "appuser:${file(var.public_key_path)}"  
+    } 
+    
+    }
+
+# Правило firewall 
+resource "google_compute_firewall" "firewall_mongo" {  
+  name = "allow-mongo-default"  
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить 
+  allow {    
+      protocol = "tcp"    
+      ports = ["27017"]  
+  }  
+  
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-db"]  
+  
+  # Порт будет доступен только для машин со след. тегом:
+  source_tags = ["reddit-app"]
+  
+}
