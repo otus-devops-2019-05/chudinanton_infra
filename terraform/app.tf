fd8582ed4737e4df5118d0a0b23d3405e34bad4a
Anton Chudin <chudinanton@gmail.com>
2019-04-03 09:52:43 +0300

    configuration separation

---
+++ terraform/app.tf
@@ -0,0 +1,49 @@
+resource "google_compute_instance" "app" {
+  name         = "reddit-app${count.index}"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+  count        = "${var.count}"
+
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  metadata {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+}
+
+resource "google_compute_address" "app_ip" {  
+  name = "reddit-app-ip"
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+
+  # Название сети, в которой действует правило
+  network = "default"
+
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-terraform {
-  # Версия terraform
-  required_version = "0.11.13"
-}
-
 provider "google" {
   # Версия провайдера
   version = "2.0.0"
@@ -12,86 +7,9 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_project_metadata" "default" {
-  metadata = {
-    ssh-keys = "appuser1:${file(var.public_key_path_appuser1)}\nappuser2:${file(var.public_key_path_appuser2)}\nappuser3:${file(var.public_key_path_appuser3)}"
-  }
-}
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app${count.index}"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  tags         = ["reddit-app"]
-  count        = "${var.count}"
 
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
 
-  metadata {
-    # путь до публичного ключа
-    ssh-keys = "appuser:${file(var.public_key_path)}"
-  }
 
-  network_interface {
-    network = "default"
 
-    access_config {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
 
-  connection {
-    type  = "ssh"
-    user  = "appuser"
-    agent = false
-
-    # путь до приватного ключа
-    private_key = "${file("~/.ssh/appuser")}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-
-  # Название сети, в которой действует правило
-  network = "default"
-
-  # Какой доступ разрешить
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  # Каким адресам разрешаем доступ
-  source_ranges = ["0.0.0.0/0"]
-
-  # Правило применимо для инстансов с перечисленными тэгами
-  target_tags = ["reddit-app"]
-}
-
-resource "google_compute_firewall" "firewall_ssh" {  
-  name = "default-allow-ssh"  
-  network = "default"  
-  allow {    
-    protocol = "tcp"    
-    ports = ["22"]  
-    }  
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_address" "app_ip" {  
-  name = "reddit-app-ip"
-}
